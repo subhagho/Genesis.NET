6d8b346d0fc587c0b40a1750b1cf32145e45df3a
Subho Ghosh <subho.ghosh@outlook.com>
2019-03-27 08:43:47 +0530

    Initial code commit.
    Sync commit.

---
+++ Genesis/LibGenesisCommon/Src/Pipeline.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using LibZConfig.Common.Utils;
+
+namespace LibGenesisCommon.Process
+{
+    /// <summary>
+    /// Exception class to be used to propogate process errors.
+    /// </summary>
+    public class ProcessException : Exception
+    {
+        private static readonly string __PREFIX = "Process Error : {0}";
+
+        /// <summary>
+        /// Constructor with error message.
+        /// </summary>
+        /// <param name="mesg">Error message</param>
+        public ProcessException(string mesg) : base(String.Format(__PREFIX, mesg))
+        {
+
+        }
+
+        /// <summary>
+        /// Constructor with error message and cause.
+        /// </summary>
+        /// <param name="mesg">Error message</param>
+        /// <param name="cause">Cause</param>
+        public ProcessException(string mesg, Exception cause) : base(String.Format(__PREFIX, mesg), cause)
+        {
+
+        }
+
+        /// <summary>
+        /// Constructor with cause.
+        /// </summary>
+        /// <param name="exception">Cause</param>
+        public ProcessException(Exception exception) : base(String.Format(__PREFIX, exception.Message), exception)
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// Exception class to be used to propogate process errors.
+    /// </summary>
+    public class ProcessUnhandledException : Exception
+    {
+        private static readonly string __PREFIX = "Unhandled Exception : {0}";
+
+        /// <summary>
+        /// Constructor with error message.
+        /// </summary>
+        /// <param name="mesg">Error message</param>
+        public ProcessUnhandledException(string mesg) : base(String.Format(__PREFIX, mesg))
+        {
+
+        }
+
+        /// <summary>
+        /// Constructor with error message and cause.
+        /// </summary>
+        /// <param name="mesg">Error message</param>
+        /// <param name="cause">Cause</param>
+        public ProcessUnhandledException(string mesg, Exception cause) : base(String.Format(__PREFIX, mesg), cause)
+        {
+
+        }
+
+        /// <summary>
+        /// Constructor with cause.
+        /// </summary>
+        /// <param name="exception">Cause</param>
+        public ProcessUnhandledException(Exception exception) : base(String.Format(__PREFIX, exception.Message), exception)
+        {
+
+        }
+    }
+
+    public class Pipeline<T>
+    {
+        protected List<Processor<T>> processors = new List<Processor<T>>();
+
+        public ProcessResponse<T> Execute(T data)
+        {
+            LogUtils.Debug("Running Process Pipeline:", data);
+            ProcessResponse<T> response = new ProcessResponse<T>();
+            response.Data = data;
+            if (processors.Count > 0)
+            {
+                try
+                {
+                    foreach (Processor<T> processor in processors)
+                    {
+                        response = processor.Execute(data);
+                        if (response.State == EProcessResponse.FatalError)
+                        {
+                            Exception ex = response.GetError();
+                            if (ex == null)
+                            {
+                                ex = new ProcessException("Processor raised error.");
+                            }
+                            throw new ProcessException(ex);
+                        }
+                        else if (response.State == EProcessResponse.ContinueWithError)
+                        {
+                            Exception ex = response.GetError();
+                            if (ex != null)
+                            {
+                                LogUtils.Error(String.Format("Continuing with error: [type={0}]", processor.GetType().FullName));
+                                LogUtils.Error(ex);
+                            }
+                            else
+                            {
+                                LogUtils.Error(String.Format("Continuing with error: [type={0}]", processor.GetType().FullName));
+                            }
+                        }
+                        else if (response.State == EProcessResponse.StopWithOk)
+                        {
+                            LogUtils.Warn(String.Format("Terminating further processing: [type={0}][reponse={1}]", processor.GetType().FullName, response.State.ToString()));
+                            break;
+                        }
+                        else if (response.State == EProcessResponse.StopWithError)
+                        {
+                            Exception ex = response.GetError();
+                            if (ex != null)
+                            {
+                                LogUtils.Error(String.Format("Stopping with error: [type={0}]", processor.GetType().FullName));
+                                LogUtils.Error(ex);
+                            }
+                            else
+                            {
+                                LogUtils.Error(String.Format("Stopping with error: [type={0}]", processor.GetType().FullName));
+                            }
+                            break;
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    LogUtils.Error(e);
+                    response.SetError(e);
+                    if (e.GetType() == typeof(ProcessUnhandledException))
+                    {
+                        response.State = EProcessResponse.UnhandledError;
+                    }
+                }
+            }
+            return response;
+        }
+    }
+}
---
+++ Genesis/LibGenesisCommon/Src/Processor.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using LibZConfig.Common;
+
+namespace LibGenesisCommon.Process
+{
+    /// <summary>
+    /// Enum for process response.
+    /// </summary>
+    public enum EProcessResponse
+    {
+        /// <summary>
+        /// Not defined.
+        /// </summary>
+        None,
+        /// <summary>
+        /// Success
+        /// </summary>
+        OK,
+        /// <summary>
+        /// Fatal Error: Terminate
+        /// </summary>
+        FatalError,
+        /// <summary>
+        /// Unknown/Unhandled Error: Terminate
+        /// </summary>
+        UnhandledError,
+        /// <summary>
+        /// Step raised error but OK to continue
+        /// </summary>
+        ContinueWithError,
+        /// <summary>
+        /// Step finished successfully, but stop further processing.
+        /// </summary>
+        StopWithOk,
+        /// <summary>
+        /// Step raised error, stop further processing.
+        /// </summary>
+        StopWithError
+    }
+
+    /// <summary>
+    /// Response object to be returned by each processor.
+    /// </summary>
+    /// <typeparam name="T">Type of the response entity</typeparam>
+    public class ProcessResponse<T> : AbstractState<EProcessResponse>
+    {
+        /// <summary>
+        /// Response Entity
+        /// </summary>
+        public T Data { get; set; }
+
+        /// <summary>
+        /// Default Empty constructor
+        /// </summary>
+        public ProcessResponse()
+        {
+            State = EProcessResponse.None;
+        }
+
+        /// <summary>
+        /// Get the error state for this type.
+        /// </summary>
+        /// <returns></returns>
+        public override EProcessResponse GetErrorState()
+        {
+            return EProcessResponse.FatalError;
+        }
+    }
+
+    /// <summary>
+    /// Interface to be implmenented by processors.
+    /// </summary>
+    /// <typeparam name="T">Entity type this processor handles.</typeparam>
+    public interface Processor<T>
+    {
+        /// <summary>
+        /// Execute the processor on the specified entity data.
+        /// </summary>
+        /// <param name="data">Entity data input</param>
+        /// <returns>Response</returns>
+        ProcessResponse<T> Execute(T data);
+    }
+}
